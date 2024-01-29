1713e4a26c101812db6741fe2c7fe36ddb83bd90
YoussefGhollamallah <youssef.gh@live.fr>
2024-01-29 21:38:16 +0100

    update ajout des commentaire

--- jour01/job19.sql
+++ jour01/job19.sql
@@ -1,5 +1,5 @@
--- premiere version
+-- premiere version / compte le nombre d'étudiant grâce à la méthode count()
 SELECT COUNT(*) FROM etudiants;
 
--- deuxièeme version
+-- deuxièeme version / compte le nombre d'étudiant grâce à la function count() en donnant le nom nombre_etudiants à la table
 SELECT COUNT(*) AS nombre_etudiants FROM etudiants;
